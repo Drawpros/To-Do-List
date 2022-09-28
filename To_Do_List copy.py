a4dcfd7c4d1440b20463b6609d3f31480691e15f
Red <95301204+Drawpros@users.noreply.github.com>
2022-09-28 15:50:06 +0200

    Add files via upload

---
+++ To_Do_List copy.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("310x400")
+
+entry = ttk.Entry(root, width = 30, font=20)
+entry.focus()
+entry.pack(ipady = 10)
+
+def new_task():
+    if entry.get() == "" or entry.get() == " ":
+        pass
+    else:
+        def del_task():
+            text.destroy()
+            checkbox.destroy()
+
+        text = tk.Text(root, width=30, height=1)
+        text.pack()
+
+        checkbox = ttk.Checkbutton(root, variable = tk.StringVar, text='Done', command=del_task)
+        checkbox.pack()
+        
+        text.insert(INSERT, f"{entry.get()}")
+        entry.delete(0, END)
+
+def nsk(event):
+    if entry.get() == "" or entry.get() == " ":
+        pass
+    else:
+        def del_task():
+            text.destroy()
+            checkbox.destroy()
+
+        text = tk.Text(root, width=30, height=1)
+        text.pack()
+        
+        checkbox = ttk.Checkbutton(root, variable = tk.StringVar, text='Done', command=del_task)
+        checkbox.pack()
+        
+        text.insert(INSERT, f"{entry.get()}")
+        entry.delete(0, END)
+
+add_task = tk.Button(root, text="Add task", height=2, width=20, font=50, command=new_task)
+root.bind('<Control-r>', nsk)
+add_task.pack(pady=5)
+
+
+root.mainloop()
---
+++ To_Do_List.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("310x400")
+
+text = tk.Text(root, width=30, height=10)
+text.pack(pady = 15)
+
+entry = ttk.Entry(root, width = 30, font=20)
+entry.focus()
+entry.pack(ipady = 10)
+
+def new_task():
+    if entry.get() == "" or entry.get() == " ":
+        pass
+    else:
+        text.insert(INSERT, f"{entry.get()} \n")
+        entry.delete(0, END)
+
+def nsk(event):
+    if entry.get() == "" or entry.get() == " ":
+        pass
+    else:
+        text.insert(INSERT, f"{entry.get()} \n")
+        entry.delete(0, END)
+
+def delete(event):
+    text.delete("end-2l","end-1l")
+    entry.delete(0, END)
+
+add_task = tk.Button(root, text="Add task", height=2, width=20, font=50, command=new_task)
+root.bind('<Control-r>', nsk)
+root.bind('<Control-t>', delete)
+add_task.pack(pady=5)
+
+def delete_last_task():
+    text.delete("end-2l","end-1l")
+    entry.delete(0, END)
+
+delete_task = tk.Button(root, text="Delete last task", height=2, width=20, font=50, command=delete_last_task)
+delete_task.pack(pady=5)
+
+
+root.mainloop()
